8d9714d737771cfc8c27e62388d58b37b7dc33fa
Roman Galeev <roman.galeev@ezuce.com>
2018-02-07 17:33:21 +0100

    Fix path

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ do
 	cd $FOLDER && ./run.sh && cd ../
 done
 
-cd nginx && ./build.sh && ./run.sh
+cd nginx && ./build.sh && ./run.sh && cd ../
 
 FS_IP=`bin/get-ip freeswitch.$NETWORK`
 sudo /sbin/iptables -t nat -A PREROUTING -i eth0 -p udp -m udp --dport 10000:12000 -j DNAT --to-destination $FS_IP
