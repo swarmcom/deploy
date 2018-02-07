fe09c6705c1caccebe789b4e235c54a3b144946a
Roman Galeev <roman.galeev@ezuce.com>
2018-02-07 16:49:15 +0100

    Create key storage and remove reach3 example cfg

--- rhel-host-setup.sh
+++ rhel-host-setup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-yum -y install yum-utils device-mapper-persistent-data lvm2 git ngrep tcpdump
+yum -y install yum-utils device-mapper-persistent-data lvm2 git ngrep tcpdump net-tools
 yum -y update
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum -y install docker-ce
 yum -y install docker-ce
 systemctl start docker
 adduser ezuce
-chown ezuce:ezuce -R /home/ezuce
 usermod -a -G docker ezuce
+mkdir -p /home/ezuce/keys
+mkdir -p /home/ezuce/keys-challenge/.well-known/acme-challenge
+chown ezuce:ezuce -R /home/ezuce
+
