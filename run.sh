09468f6b81956980c2c4f4a6635db5ad57b6432b
Roman Galeev <roman.galeev@ezuce.com>
2018-02-07 15:18:27 +0100

    Add freeswitch iptables rule

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ done
 
 cd nginx && ./build.sh && ./run.sh
 
+FS_IP=`bin/get-ip freeswitch.reach3`
+/sbin/iptables -t nat -A PREROUTING -i eth0 -p udp -m udp --dport 10000:12000 -j DNAT --to-destination $FS_IP
