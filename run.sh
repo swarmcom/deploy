95fb4c773f29814771e385902c0864e77f3da582
Roman Galeev <roman.galeev@ezuce.com>
2018-02-09 23:33:37 +0100

    Extra rules to handle rtp/sip packets

--- run.sh
+++ run.sh
@@ -23,8 +23,12 @@ done
 
 cd nginx && ./build.sh && ./run.sh && cd ../
 
+# This extra rules we need somehow to manage to be able dynamically add containers
+EXT_IP=${EXT_IP:-"$(curl -4 -s ifconfig.co)"}
 FS_IP=`bin/get-ip freeswitch.$NETWORK`
 sudo /sbin/iptables -t nat -A PREROUTING -i eth0 -p udp -m udp --dport 10000:12000 -j DNAT --to-destination $FS_IP
+KAM_IP=`bin/get-ip kamailio.$NETWORK`
+sudo /sbin/iptables -t nat -A PREROUTING -p udp -m udp -d $EXT_IP --dport 5060 -j DNAT --to-destination $KAM_IP
 
 if [ -z $USE_LE ]
 then
