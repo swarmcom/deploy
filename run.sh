6fd054bc82c06a21e9fcd837824a93764301a335
Pavel Farkas <pavel.farkas@ezuce.com>
2019-04-11 11:58:57 +0200

    Run cmd for Cedarville test

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ then
 fi
 
 echo Update images
-for IMAGE in $HUB/reach $HUB/reach-ui nginx $HUB/kamailio $HUB/freeswitch $HUB/rr $HUB/timescale
+for IMAGE in $HUB/reach $HUB/reach-ui nginx $HUB/kamailio:REACH-59 $HUB/freeswitch $HUB/rr $HUB/timescale
 do
 	docker pull $IMAGE
 done
